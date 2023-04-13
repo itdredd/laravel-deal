e186c0fa73b3e33f4a37e6518a5afdca21928cfe
DREDD <mrmagnusrus@gmail.com>
2023-04-13 20:01:56 +0300

    use pusherjs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,17 +18,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+// resources/assets/js/bootstrap.js
 
-// import Pusher from 'pusher-js';
-// window.Pusher = Pusher;
+import Echo from "laravel-echo"
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     wsHost: import.meta.env.VITE_PUSHER_HOST ? import.meta.env.VITE_PUSHER_HOST : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-//     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'ff7c9524207abac1cd2a',
+    cluster: 'eu',
+    encrypted: true
+});
