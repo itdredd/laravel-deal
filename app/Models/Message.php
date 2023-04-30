253baec7133bc284de5d3a4a0049fb061e759134
DREDD <mrmagnusrus@gmail.com>
2023-04-30 19:03:25 +0300

    add phpdoc for message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,15 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @property int $id
+ * @property int $created_at
+ * @property int $updated_at
+ * @property int $deal_id
+ * @property int $user_id
+ * @property string $message
+ * @property string $status
+ */
+
 class Message extends Model
 {
     use HasFactory;
 
     protected $with = ['user'];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo|User
+     */
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo|Deal
+     */
     public function deal() {
         return $this->belongsTo(Deal::class);
     }
