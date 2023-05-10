e2809e7fdcfd575e6d907785556246b9bee6fc11
DREDD <mrmagnusrus@gmail.com>
2023-05-10 22:35:30 +0300

    fix repository query

--- app/Repository/DealRepository.php
+++ app/Repository/DealRepository.php
@@ -10,18 +10,19 @@ class DealRepository
     public function findForUser(User $user, $status = '')
     {
         if (!$status) {
-            $deals = Deal::where('author_id', $user->id)
-                    ->orWhere('members_id', $user->id)
-                    ->orWhere('guarantor_id', $user->id)
-                    ->where('status', '<>', 'rejected')
-                    ->orderByDesc('created_at');;
+            $deals = Deal::where('status', '<>', 'rejected')
+                    ->where(function ($query) use ($user) {
+                        $query->where('author_id', $user->id)
+                                ->orWhere('members_id', $user->id)
+                                ->orWhere('guarantor_id', $user->id);
+                    })->orderByDesc('created_at');
         } else {
-            $deals = Deal::where('status', $status)->where(function ($query) use ($user) {
-                $query->where('author_id', $user->id)
-                        ->orWhere('members_id', $user->id)
-                        ->orWhere('guarantor_id', $user->id)
-                        ->orderByDesc('created_at');
-            });
+            $deals = Deal::where('status', $status)
+                    ->where(function ($query) use ($user) {
+                        $query->where('author_id', $user->id)
+                                ->orWhere('members_id', $user->id)
+                                ->orWhere('guarantor_id', $user->id);
+            })->orderByDesc('created_at');
         }
         return $deals->get();
     }
